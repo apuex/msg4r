eb40f271a850e260d594e50c531a165575defcdf
Wangxy <xtwxy@hotmail.com>
2021-03-23 08:26:28 +0800

    change map serialization to boost::serialization.

--- test/msg4r-binary-test.cpp
+++ test/msg4r-binary-test.cpp
@@ -2,21 +2,17 @@
 #include <boost/test/included/unit_test.hpp>
 
 #include <boost/serialization/nvp.hpp>
-#include <boost/archive/text_iarchive.hpp>
-#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/version.hpp>
 
 #include <algorithm>
 #include <ostream>
 
 typedef struct my_struct {
-  friend class boost::serialization::access;
-  friend std::ostream& operator<<(std::ostream& os, const my_struct& t);
-  template <typename Archive>
-  void serialize(Archive& ar, const unsigned int version) {
-    ar& BOOST_SERIALIZATION_NVP(i);
-    ar& BOOST_SERIALIZATION_NVP(m);
-  }
-
+  // friend class boost::serialization::access;
+  // friend std::ostream& operator<<(std::ostream& os, const my_struct& t);
   explicit my_struct() : i() {}
   explicit my_struct(int iv, std::map<std::string, std::string> mv) : i(iv), m(mv) {}
   explicit my_struct(const my_struct& t) : i(t.i), m(t.m) {}
@@ -54,6 +50,14 @@ namespace boost {
 namespace serialization {
 
 template <typename Archive>
+void serialize(Archive& ar, 
+    my_struct& t,
+    const unsigned int version) {
+  ar& BOOST_SERIALIZATION_NVP(t.i);
+  ar& BOOST_SERIALIZATION_NVP(t.m);
+}
+
+template <typename Archive>
 void serialize(
     Archive& ar,
     std::map<std::string, std::string>& t,
@@ -67,8 +71,10 @@ void serialize(
 } // namespace serialization
 } // namespace boost
 
+BOOST_CLASS_VERSION(my_struct, 0x7e)
+
 BOOST_AUTO_TEST_CASE(first_test) {
-  int i = 100; 
+  int i = 0xdead; 
   std::map<std::string, std::string> m{
       {"hello", "world"},
       {"fuck", "Donald Jr. Trump"},
@@ -76,7 +82,7 @@ BOOST_AUTO_TEST_CASE(first_test) {
   my_struct_t s1{i,m};
   my_struct_t s2{i,m};
   //std::cout << s1;
-  boost::archive::text_oarchive oa(std::cout);
+  boost::archive::binary_oarchive oa(std::cout);
   oa << s1;
   BOOST_TEST(s1 == s2);
 }
