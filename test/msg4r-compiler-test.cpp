307315ae316cf7399b8467c378a4d62cb41e4be1
Wangxy <xtwxy@hotmail.com>
2021-03-23 14:35:38 +0800

    change %zd to %d

--- test/msg4r-compiler-test.cpp
+++ test/msg4r-compiler-test.cpp
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]) {
 	printf("%s\n", argv[0]);
 
 #ifdef _MSC_VER
-  printf("_MSC_VER = %zd\n", _MSC_VER);
+  printf("_MSC_VER = %d\n", _MSC_VER);
   printf("sizeof(UINT) = %zd\n", sizeof(UINT));
 #endif
   printf("sizeof(uint8_t) = %zd\n", sizeof(uint8_t));
