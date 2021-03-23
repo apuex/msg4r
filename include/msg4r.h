8f1929d45219c06bb4c9ce3ac4ef21326cfbd757
Wangxy <xtwxy@hotmail.com>
2021-03-23 23:29:21 +0800

    implement endianness

--- include/msg4r.h
+++ include/msg4r.h
@@ -10,45 +10,81 @@
 
 namespace msg4r {
 
-std::istream& read(std::istream& is,  int8_t& v);
-std::ostream& write(std::ostream& os, int8_t& v);
-std::istream& read(std::istream& is,  int16_t& v);
-std::ostream& write(std::ostream& os, int16_t& v);
-std::istream& read(std::istream& is,  int32_t& v);
-std::ostream& write(std::ostream& os, int32_t& v);
-std::istream& read(std::istream& is,  int64_t& v);
-std::ostream& write(std::ostream& os, int64_t& v);
-
-std::istream& read(std::istream& is,  uint8_t& v);
-std::ostream& write(std::ostream& os, uint8_t& v);
-std::istream& read(std::istream& is,  uint16_t& v);
-std::ostream& write(std::ostream& os, uint16_t& v);
-std::istream& read(std::istream& is,  uint32_t& v);
-std::ostream& write(std::ostream& os, uint32_t& v);
-std::istream& read(std::istream& is,  uint64_t& v);
-std::ostream& write(std::ostream& os, uint64_t& v);
+#ifdef BIG_ENDIAN
+#define from_native boost::endian::native_to_big
+#define to_native boost::endian::big_to_native
+#else
+#define from_native boost::endian::native_to_little
+#define to_native boost::endian::little_to_native
+#endif
 
 std::istream& read(std::istream& is, std::string& v);
 std::ostream& write(std::ostream& os, const std::string& v);
 
 template<typename T>
-std::istream& read(std::istream& is, std::vector<T>& v);
-template<typename T>
-std::ostream& write(std::ostream& os, const std::vector<T>& v);
+std::istream& read(std::istream& is, T& v) {
+  T t;
+  is.read((char*)&t, sizeof(t));
+  v = to_native(t);
+  return is;
+}
 
 template<typename T>
-std::istream& read(std::istream& is, std::list<T>& v);
-template<typename T>
-std::ostream& write(std::ostream& os, const std::list<T>& v);
+std::ostream& write(std::ostream& os, const T& v) {
+  T t = from_native(v);
+  os.write((char*)&t, sizeof(t));
+  return os;
+}
 
 template<typename T>
-std::istream& read(std::istream& is, std::set<T>& v);
+std::istream& read(std::istream& is, std::vector<T>& v) {
+  return is;
+}
+
 template<typename T>
-std::ostream& write(std::ostream& os, const std::set<T>& v);
+std::ostream& write(std::ostream& os, const std::vector<T>& v) {
+  uint32_t length = static_cast<uint32_t>(v.length());
+  write(os, length);
+  std::for_each(v.begin(), v.end(), [&](auto e) {
+      write(os, e);
+    });
+  return os;
+}
+
+template<typename T>
+std::istream& read(std::istream& is, std::list<T>& v) {
+  return is;
+}
+
+template<typename T>
+std::ostream& write(std::ostream& os, const std::list<T>& v) {
+  uint32_t length = static_cast<uint32_t>(v.length());
+  write(os, length);
+  std::for_each(v.begin(), v.end(), [&](auto e) {
+      write(os, e);
+    });
+  return os;
+}
+
+template<typename T>
+std::istream& read(std::istream& is, std::set<T>& v) {
+  return is;
+}
 
 template<typename K, typename V>
-std::istream& read(std::istream& is, std::map<K, V>& v);
+std::istream& read(std::istream& is, std::map<K, V>& v) {
+  return is;
+}
+
 template<typename K, typename V>
-std::ostream& write(std::ostream& os, const std::map<K, V>& v);
+std::ostream& write(std::ostream& os, const std::map<K, V>& v) {
+  uint32_t length = static_cast<uint32_t>(v.length());
+  write(os, length);
+  std::for_each(v.begin(), v.end(), [&](auto& e) {
+      write(os, e.first);
+      write(os, e.second);
+    });
+  return os;
+}
 
 }
--- src/msg4r.cpp
+++ src/msg4r.cpp
@@ -1,134 +1,19 @@
 #include <msg4r.h>
+#include <algorithm>
+#include <boost/endian/conversion.hpp>
 
 namespace msg4r {
-
-std::istream& read(std::istream& is,  int8_t& v) {
-  is.read((char*)&v, sizeof(v));
-  return is;
-}
-
-std::ostream& write(std::ostream& os, int8_t& v) {
-  os.write((char*)&v, sizeof(v));
-  return os;
-}
-
-std::istream& read(std::istream& is,  int16_t& v) {
-  is.read((char*)&v, sizeof(v));
-  return is;
-}
-
-std::ostream& write(std::ostream& os, int16_t& v) {
-  os.write((char*)&v, sizeof(v));
-  return os;
-}
-
-std::istream& read(std::istream& is,  int32_t& v) {
-  is.read((char*)&v, sizeof(v));
-  return is;
-}
-
-std::ostream& write(std::ostream& os, int32_t& v) {
-  os.write((char*)&v, sizeof(v));
-  return os;
-}
-
-std::istream& read(std::istream& is,  int64_t& v) {
-  is.read((char*)&v, sizeof(v));
-  return is;
-}
-
-std::ostream& write(std::ostream& os, int64_t& v) {
-  os.write((char*)&v, sizeof(v));
-  return os;
-}
-
-
-std::istream& read(std::istream& is,  uint8_t& v) {
-  is.read((char*)&v, sizeof(v));
-  return is;
-}
-
-std::ostream& write(std::ostream& os, uint8_t& v) {
-  os.write((char*)&v, sizeof(v));
-  return os;
-}
-
-std::istream& read(std::istream& is,  uint16_t& v) {
-  is.read((char*)&v, sizeof(v));
-  return is;
-}
-
-std::ostream& write(std::ostream& os, uint16_t& v) {
-  os.write((char*)&v, sizeof(v));
-  return os;
-}
-
-std::istream& read(std::istream& is,  uint32_t& v) {
-  is.read((char*)&v, sizeof(v));
-  return is;
-}
-
-std::ostream& write(std::ostream& os, uint32_t& v) {
-  os.write((char*)&v, sizeof(v));
-  return os;
-}
-
-std::istream& read(std::istream& is,  uint64_t& v) {
-  is.read((char*)&v, sizeof(v));
-  return is;
-}
-
-std::ostream& write(std::ostream& os, uint64_t& v) {
-  os.write((char*)&v, sizeof(v));
-  return os;
-}
-
 
 std::istream& read(std::istream& is, std::string& v) {
   return is;
 }
 
 std::ostream& write(std::ostream& os, const std::string& v) {
-  return os;
-}
-
-template<typename T>
-std::istream& read(std::istream& is, std::vector<T>& v) {
-  return is;
-}
-
-template<typename T>
-std::ostream& write(std::ostream& os, const std::vector<T>& v) {
-  return os;
-}
-
-template<typename T>
-std::istream& read(std::istream& is, std::list<T>& v) {
-  return is;
-}
-
-template<typename T>
-std::ostream& write(std::ostream& os, const std::list<T>& v) {
-  return os;
-}
-
-template<typename T>
-std::istream& read(std::istream& is, std::set<T>& v) {
-  return is;
-}
-
-template<typename T>
-std::ostream& write(std::ostream& os, const std::set<T>& v) {
-  return os;
-}
-
-template<typename K, typename V>
-std::istream& read(std::istream& is, std::map<K, V>& v) {
-  return is;
-}
-
-template<typename K, typename V>
-std::ostream& write(std::ostream& os, const std::map<K, V>& v) {
+  uint32_t length = static_cast<uint32_t>(v.length());
+  write(os, length);
+  std::for_each(v.begin(), v.end(), [&](auto& e) {
+      write(os, e);
+    });
   return os;
 }
 
