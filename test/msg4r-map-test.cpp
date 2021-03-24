7673d30dee0bfb8ca552f01e634ccf3ff6a2ca5c
Wangxy <xtwxy@hotmail.com>
2021-03-24 15:23:43 +0800

    implement map test

--- test/msg4r-map-test.cpp
+++ test/msg4r-map-test.cpp
@@ -12,7 +12,7 @@ BOOST_AUTO_TEST_CASE(string_test) {
   std::string str = ssm.str();
   std::for_each(str.begin(), str.end(), [&](auto& e) {
       std::cout << std::hex << std::uppercase << std::setw(2) << std::setfill('0')
-          << static_cast<uint32_t>(e) << " ";
+          << (static_cast<uint32_t>(e) & 0xff) << " ";
     });
     std::cout << std::endl;
   msg4r::read(ssm, s2);
@@ -27,7 +27,7 @@ BOOST_AUTO_TEST_CASE(string_test) {
   str = ssm.str();
   std::for_each(str.begin(), str.end(), [&](auto& e) {
       std::cout << std::hex << std::uppercase << std::setw(2) << std::setfill('0')
-          << static_cast<uint32_t>(e) << " ";
+          << (static_cast<uint32_t>(e) & 0xff) << " ";
     });
     std::cout << std::endl;
   msg4r::read(ssm, s2);
@@ -36,3 +36,74 @@ BOOST_AUTO_TEST_CASE(string_test) {
     });
   BOOST_TEST(s1 == s2);
 }
+
+BOOST_AUTO_TEST_CASE(uint16_test) {
+  std::map<uint16_t, uint16_t> s1 = { {0xcafe, 0xbeef} };
+  std::map<uint16_t, uint16_t> s2;
+  std::stringstream ssm;
+  msg4r::write(ssm, s1);
+  std::string str = ssm.str();
+  std::for_each(str.begin(), str.end(), [&](auto& e) {
+      std::cout << std::hex << std::uppercase << std::setw(2) << std::setfill('0')
+          << (static_cast<uint32_t>(e) & 0xff) << " ";
+    });
+    std::cout << std::endl;
+  msg4r::read(ssm, s2);
+  std::for_each(s2.begin(), s2.end(), [&](auto& e) {
+      std::cout << std::hex << std::uppercase << std::setw(8) << std::setfill('0')
+          << "(" << e.first << ", " << e.second << ")" << std::endl;
+    });
+  BOOST_TEST(s1 == s2);
+  s1 = { { 0xdead, 0xface } };
+  s2.clear();
+  ssm.str("");
+  msg4r::write(ssm, s1);
+  str = ssm.str();
+  std::for_each(str.begin(), str.end(), [&](auto& e) {
+      std::cout << std::hex << std::uppercase << std::setw(2) << std::setfill('0')
+          << (static_cast<uint32_t>(e) & 0xff) << " ";
+    });
+    std::cout << std::endl;
+  msg4r::read(ssm, s2);
+  std::for_each(s2.begin(), s2.end(), [&](auto& e) {
+      std::cout << std::hex << std::uppercase << std::setw(8) << std::setfill('0')
+          << "(" << e.first << ", " << e.second << ")" << std::endl;
+    });
+  BOOST_TEST(s1 == s2);
+}
+
+BOOST_AUTO_TEST_CASE(uint32_test) {
+  std::map<uint32_t, uint32_t> s1 = { {0xcafe, 0xbeef} };
+  std::map<uint32_t, uint32_t> s2;
+  std::stringstream ssm;
+  msg4r::write(ssm, s1);
+  std::string str = ssm.str();
+  std::for_each(str.begin(), str.end(), [&](auto& e) {
+      std::cout << std::hex << std::uppercase << std::setw(2) << std::setfill('0')
+          << (static_cast<uint32_t>(e) & 0xff) << " ";
+    });
+    std::cout << std::endl;
+  msg4r::read(ssm, s2);
+  std::for_each(s2.begin(), s2.end(), [&](auto& e) {
+      std::cout << std::hex << std::uppercase << std::setw(8) << std::setfill('0')
+          << "(" << e.first << ", " << e.second << ")" << std::endl;
+    });
+  BOOST_TEST(s1 == s2);
+  s1 = { { 0xdead, 0xface } };
+  s2.clear();
+  ssm.str("");
+  msg4r::write(ssm, s1);
+  str = ssm.str();
+  std::for_each(str.begin(), str.end(), [&](auto& e) {
+      std::cout << std::hex << std::uppercase << std::setw(2) << std::setfill('0')
+          << (static_cast<uint32_t>(e) & 0xff) << " ";
+    });
+    std::cout << std::endl;
+  msg4r::read(ssm, s2);
+  std::for_each(s2.begin(), s2.end(), [&](auto& e) {
+      std::cout << std::hex << std::uppercase << std::setw(8) << std::setfill('0')
+          << "(" << e.first << ", " << e.second << ")" << std::endl;
+    });
+  BOOST_TEST(s1 == s2);
+}
+
