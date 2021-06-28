0abfb1ffb3f99837ad6c7b1369973bfc7b06d5e3
Wangxy <xtwxy@hotmail.com>
2021-06-28 19:53:55 +0800

    add more field types.

--- test/msg4r-json-test.cpp
+++ test/msg4r-json-test.cpp
@@ -6,25 +6,31 @@
 #include <boost/json/src.hpp>
 
 typedef struct my_struct {
-  my_struct() : i() {}
-  my_struct(int v) : type_url("type.googleapis.com/my_struct"), i(v) {}
-  my_struct(const my_struct& t) : type_url(t.type_url), i(t.i) {}
+  my_struct() : type_url_("type.googleapis.com/my_struct"), b_(), i_(), f_() {}
+  my_struct(bool v) : type_url_("type.googleapis.com/my_struct/bool"), b_(v), i_(), f_() {}
+  my_struct(int v) : type_url_("type.googleapis.com/my_struct/int"), b_(), i_(v), f_() {}
+  my_struct(double v) : type_url_("type.googleapis.com/my_struct/double"), b_(), i_(), f_(v) {}
+  my_struct(const my_struct& t) : type_url_(t.type_url_), b_(t.b_), i_(t.i_), f_(t.f_) {}
 
   my_struct& operator=(const my_struct& t) { 
-    this->i = t.i;
+    this->b_ = t.b_;
+    this->i_ = t.i_;
+    this->f_ = t.f_;
     return *this;
   }
 
   bool operator==(const my_struct& t) const { 
-    return (this->i == t.i);
+    return std::tie(type_url_, b_, i_, f_) == std::tie(t.type_url_, t.b_, t.i_, t.f_);
   }
 
-  const std::string type_url;
-  int i;
+  const std::string type_url_;
+  bool b_;
+  int i_;
+  double f_;
 } my_struct_t;
 
 std::ostream& operator<<(std::ostream& os, const my_struct& t) {
-  os << "struct my_struct { type_url = \"" << t.type_url << "\", i = " << t.i << " }";
+  os << "struct my_struct { type_url = \"" << t.type_url_ << "\", i = " << t.i_ << " }";
   return os;
 }
 
@@ -33,8 +39,10 @@ void tag_invoke(
   boost::json::value& jv,
   my_struct_t const & t) {
   jv = {
-    {"@type", t.type_url },
-    {"i", t.i }
+    {"@type", t.type_url_ },
+    {"b", t.b_ },
+    {"i", t.i_ },
+    {"f", t.f_ }
   };
 }
 
@@ -42,10 +50,15 @@ my_struct_t tag_invoke(
   boost::json::value_to_tag<my_struct_t>,
   const boost::json::value& jv) {
   boost::json::object const& obj = jv.as_object();
-  if(obj.at("@type") == "type.googleapis.com/my_struct") {
-    std::cout << "@type matched." << std::endl;
+  if(obj.at("@type") == "type.googleapis.com/my_struct/bool") {
+    return my_struct_t(boost::json::value_to<bool>(obj.at("b")));
+  } else if(obj.at("@type") == "type.googleapis.com/my_struct/int") {
+    return my_struct_t(boost::json::value_to<int>(obj.at("i")));
+  } else if(obj.at("@type") == "type.googleapis.com/my_struct/double") {
+    return my_struct_t(boost::json::value_to<double>(obj.at("f")));
+  } else {
+    return my_struct_t(boost::json::value_to<int>(obj.at("i")));
   }
-  return my_struct_t(boost::json::value_to<int>(obj.at("i")));
 }
 
 template <typename T>
@@ -54,14 +67,15 @@ void check(T t) {
 }
 
 BOOST_AUTO_TEST_CASE(first_test) {
-  int i = 1;
+  double i = 1234;
   my_struct_t s1{i};
   my_struct_t s2{i};
   BOOST_TEST(s1 == s2);
   check(i);
   check(s1);
-  std::cout << boost::json::serialize(boost::json::value_from(s1)) << std::endl;
-  boost::json::value jv = boost::json::parse("{\"@type\":\"type.googleapis.com/my_struct\",\"i\":1}");
+  auto str = boost::json::serialize(boost::json::value_from(s1));
+  std::cout << str << std::endl;
+  boost::json::value jv = boost::json::parse(str);
   s2 = boost::json::value_to<my_struct_t>(jv);
   BOOST_TEST(s1 == s2);
 }
